7669f493041ce18acd0f919b4b012d8cd968de0c
Flen-E <cjj642@naver.com>
2023-08-20 01:00:17 +0900

    commit change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Prac from './Prac';
 import App from './App';
+import { ThemeProvider } from "styled-components";
+
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+
   </React.StrictMode>
 );
 
